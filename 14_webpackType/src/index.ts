81c8da838ac74ab3590920e6379f29538b1fcff5
iMac <imac@iMac-iMac.local>
2023-03-14 21:47:19 +0300

    fix: add

---
+++ 14_webpackType/src/index.ts
@@ -0,0 +1,13 @@
+import { formData } from './forms';
+
+const form = document.querySelector('form')!;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = formData(form);
+  console.log(data);
+});
+
+
+
+console.log([null].length)
